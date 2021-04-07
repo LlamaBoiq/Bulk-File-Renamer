df5364b704f03d8bf831712189b0051e76ea9b9d
LlamaBoiq <assassinscreedllama123@gmail.com>
2021-04-07 13:52:29 -0500

    Update bulkfilerenamer.py

--- bulkfilerenamer.py
+++ bulkfilerenamer.py
@@ -1,45 +1,47 @@
 import os 
 import sys
 
+
 def main_():
     global path_n, name, decn, ftype
-    path_n = str(input("Enter full file path target to bulk rename every file: ")) # Must have "/" and not "\" 
+    path_n = str(input("Enter full folder path target to bulk rename every file: ")) # Must have "/" and not "\" 
     name = input("Enter the Name: ")
     ftype = input("Enter file extention type (e.g; .txt, .png, etc)\n>> ")
+    if ftype.startswith('.') == False:
+        print("File exention type must start with a '.' in order to work")
+        # { # Makes sure the path syntax is correct
     path_n = path_n.replace("\\", " / ")
-    if path_n.endswith("\\"):
+    if path_n.endswith("\\"):   
         path_n.endswith("/")
+        # }
 main_()
 input("Press 'enter' to bulk rename ")
 
 def _main():
     global path, name, decn, ftype, path_n
     
-
-    try:
-        path = path_n
-    except FileNotFoundError as e:
-        return
+    path = path_n
     x = 0
-    current_file = os.path.basename(sys.argv[0])
-
-   for file in os.listdir(path):
-        dest = name + str(x) + ftype
+    current_file = os.path.basename(sys.argv[0]) # Refering to this current python file
+  # { Loops through the given path
+    for file in os.listdir(path):
+        dest = name + str(x) + ftype # Name for each file
         src = path + file
         dest = path + dest
-        if file == current_file:
+        if file == current_file: # Checks if the file is this this python program
             pass
         else:
             try:
-                os.rename(src, dest)
+                os.rename(src, dest) # Renames each file
             except FileNotFoundError:
                 print("Failed to rename file. File not found error")
-            except FileExistsError:
-                print("Failed to rename file. File name already exists \n(try renaming everything to something else and then change it back)")
-            print("Successfuly renamed ", file)
+            except FileExistsError:     # Happens when the files 
+                print("error")
+            print("Successfuly renamed ", file, " --> ", dest)
         
-        x += 1
+        x += 1 # For numbering each file
+        # }
     input("Press 'enter' to exit")
 
-if __name__ == "__main__":
-    _main()
+if __name__ == "__main__": 
+    _main() 
